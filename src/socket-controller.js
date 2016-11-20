ac9ab7ec17ff447341badcee094a6eedfb9a4a0a
Ash Browning <ambrowning89@gmail.com>
2016-11-20 23:03:00 +0000

    refactor(socketio): refactoring socket logic to its own file

---
+++ src/socket-controller.js
@@ -0,0 +1,58 @@
+var crypto = require('crypto');
+var io = require('socket.io');
+
+class SocketController {
+  constructor(expressApp) {
+    this.io = io(expressApp);
+    this.initialiseSocketIO();
+  }
+
+  initialiseSocketIO() {
+
+    var _this = this;
+    this.io.on('connection', function (socket) {
+
+      _this.addNewSocketToRooms(socket);
+      _this.setupMultiplexing(socket);
+
+    });
+  }
+
+  addNewSocketToRooms(socket) {
+    //Check if it's the master coonnecting
+    var socketType = socket.handshake.query.name || null;
+    if (socketType === 'master') {
+      socket.join('realtime-vote');
+    } else if (socketType === 'client') {
+      socket.join('client');
+    }
+  }
+
+  setupMultiplexing(socket) {
+    //Send current state on connection!
+    socket.on('multiplex-statechanged', this.onMultiplexStateChange.bind(this, socket));
+  }
+
+  onMultiplexStateChange(socket, data) {
+    if (typeof data.secret == 'undefined' || data.secret == null || data.secret === '') return;
+    if (this.createHash(data.secret) === data.socketId) {
+      data.secret = null;
+      socket.broadcast.emit(data.socketId, data);
+    };
+  }
+
+  createHash(secret) {
+    var cipher = crypto.createCipher('blowfish', secret);
+    return (cipher.final('hex'));
+  }
+
+  sendRealtimeVoteUpdate(votes) {
+    //Todo - setup another channel for server -> master
+    this.io.to('realtime-vote').emit('vote-update', {
+      votes: votes
+    });
+  }
+}
+
+module.exports = SocketController;
+
